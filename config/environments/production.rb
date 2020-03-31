616e5363b5fc5abc6be8d8afa9ad211a55791033
Mario Young <maye.co@gmail.com>
2020-03-31 18:49:50 -0300

    add services & ingress

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,4 +97,5 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   config.cache_store = :redis_cache_store, { url: ENV['REDIS_URL'] }
+  config.session_store :cache_store
 end
